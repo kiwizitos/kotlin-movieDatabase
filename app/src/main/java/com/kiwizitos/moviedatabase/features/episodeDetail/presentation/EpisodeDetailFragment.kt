8e080f98841b994acd60908952468174c06c7aa8
kiwizitos <matpinheiro03@gmail.com>
2022-12-12 11:42:10 -0300

    Add episode detail fragment

---
+++ app/src/main/java/com/kiwizitos/moviedatabase/features/episodeDetail/presentation/EpisodeDetailFragment.kt
@@ -0,0 +1,12 @@
+package com.kiwizitos.moviedatabase.features.episodeDetail.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kiwizitos.moviedatabase.R
+
+class EpisodeDetailFragment : Fragment() {
+
+}
